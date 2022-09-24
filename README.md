09b22d02a423c20d1bd24237147ceb4d360fd51a
Jordan <jordan.su@protonmail.com>
2022-09-24 11:45:38 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Github Social Feed - Lambda Function
---
+++ index.js
@@ -0,0 +1,50 @@
+const { Octokit } = require("octokit")
+const aws = require('aws-sdk');
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+const s3 = new aws.S3();
+
+async function getRepoIssues(owner, repo) {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues/comments', {
+        owner: owner,
+        repo: repo
+    })
+    return data
+}
+
+exports.handler = async (event, context, handler) => {
+    let issues;
+    // const bucket = event.Records[0].s3.bucket.name;
+    const bucket = "git-repo-test-bucket";
+    // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    const key = "issues.json"
+
+    const res = await Promise.all([getRepoIssues("aws", "aws-cli"), getRepoIssues("vercel", "next.js"), getRepoIssues("facebook", "react"), getRepoIssues("torvalds", "linux")]).then(
+        results => {
+            issues = JSON.stringify({
+                aws: results[0],
+                next: resulst[1],
+                react: results[2],
+                linux: results[3]
+            })
+        }
+    )
+
+    try {
+        const destparams = {
+            Bucket: bucket,
+            Key: key,
+            Body: issues,
+            ContentType: "application/json"
+        }
+        const putResult = await s3.putObject(destparams).promise();
+
+    } catch (e) {
+        console.log(e)
+        console.log("upload error", e)
+    }
+    return issues
+}
+
+
+
